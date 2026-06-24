683eccf3260587a810866cb4fa99c1036a4238bc
proSamik <samikchoudhury15@gmail.com>
2026-06-25 01:51:11 +0530

    Initial Commit

---
+++ Viral Captions/Viral_CaptionsApp.swift
@@ -0,0 +1,32 @@
+//
+//  Viral_CaptionsApp.swift
+//  Viral Captions
+//
+//  Created by Samik Choudhury on 25/06/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Viral_CaptionsApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
